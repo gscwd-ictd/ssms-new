d14ca2f861ffd01e877040a33437695a7e47270e
Eric Sison <ericsison.dev@gmail.com>
2025-01-02 23:17:41 +0800

    added matcher for the middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,7 @@
 import { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {}
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)"],
+};
