9e3673c6b11533043801c2d9e4b9c6aa67132c0c
Eric Sison <ericsison.dev@gmail.com>
2025-01-14 23:07:25 +0800

    added dashboard ui layout

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,3 +1,11 @@
+"use client";
+import SupportDashboard from "@ssms/components/features/dashboardCards/DashboardCard";
+import { CircleCheckBig, CircleSlash2, Tickets, Timer } from "lucide-react";
+
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      <SupportDashboard />
+    </div>
+  );
 }
